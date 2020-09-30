57c06c98ee3a8a4d78ccfe38986cc3f356cff4ac
maymax777 <dev.bugger777@yahoo.com>
2020-09-30 21:46:12 +1000

    Add Team

---
+++ src/containers/Team/index.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+function Team() {
+    return (
+        <section class="team-area section-padding40 section-bg1">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-12">
+                        <div class="section-tittle text-center mb-105">
+                            <h2>Most amazing features</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <div class="single-cat">
+                            <div class="cat-icon">
+                                <img src="assets/img/icon/services1.svg" alt="" />
+                            </div>
+                            <div class="cat-cap">
+                                <h5><a href="#">Employee Owned</a></h5>
+                                <p>Supercharge your WordPress hosting with detailed website analytics, marketing tools.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <div class="single-cat">
+                            <div class="cat-icon">
+                                <img src="assets/img/icon/services2.svg" alt="" />
+                            </div>
+                            <div class="cat-cap">
+                                <h5><a href="#">Commitment to Security</a></h5>
+                                <p>Supercharge your WordPress hosting with detailed website analytics, marketing tools.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <div class="single-cat">
+                            <div class="cat-icon">
+                                <img src="assets/img/icon/services3.svg" alt="" />
+                            </div>
+                            <div class="cat-cap">
+                                <h5><a href="#">Passion for Privacy</a></h5>
+                                <p>Supercharge your WordPress hosting with detailed website analytics, marketing tools.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <div class="single-cat">
+                            <div class="cat-icon">
+                                <img src="assets/img/icon/services4.svg" alt="" />
+                            </div>
+                            <div class="cat-cap">
+                                <h5><a href="#">Employee Owned</a></h5>
+                                <p>Supercharge your WordPress hosting with detailed website analytics, marketing tools.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <div class="single-cat">
+                            <div class="cat-icon">
+                                <img src="assets/img/icon/services5.svg" alt="" />
+                            </div>
+                            <div class="cat-cap">
+                                <h5><a href="#">24/7 Support</a></h5>
+                                <p>Supercharge your WordPress hosting with detailed website analytics, marketing tools.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <div class="single-cat">
+                            <div class="cat-icon">
+                                <img src="assets/img/icon/services6.svg" alt="" />
+                            </div>
+                            <div class="cat-cap">
+                                <h5><a href="#">100% Uptime Guaranteed</a></h5>
+                                <p>Supercharge your WordPress hosting with detailed website analytics, marketing tools.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Team
