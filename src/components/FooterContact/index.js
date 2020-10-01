2815d9269f3c9c743314550c0614a5f53cd332f4
maymax777 <dev.bugger777@yahoo.com>
2020-10-01 18:54:14 +1000

    Update FooterContact

--- src/components/FooterContact/index.js
+++ src/components/FooterContact/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import FooterForm from 'components/FooterForm';
 import SocialIcons from 'components/SocialIcons';
+import { Link } from 'react-router-dom';
 
 function FooterContact() {
     return (
         <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6">
             <div className="single-footer-caption mb-50">                                    
                 <div className="footer-logo mb-25">
-                    <a href="index.html"><img src={require("assets/img/logo/logo2_footer.png")} alt="" /></a>
+                    <Link href="#"><img src={require("assets/img/logo/logo2_footer.png")} alt="" /></Link>
                 </div>
                 <div className="footer-tittle mb-50">
                     <p>Subscribe our newsletter to get updates about our services</p>
